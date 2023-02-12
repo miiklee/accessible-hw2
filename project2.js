b53d54b428bbecbddca0162213d513fc250001ed
Mirea Klee <mireaklee@Mireas-MacBook-Pro.local>
2023-02-12 16:10:45 -0500

    starting to make changes

--- project2.js
+++ project2.js
@@ -26,20 +26,32 @@ $(document).ready(function() {
 
   // Form validation
   $("#signupbutton").click(function() {
-    if($("#fn").val()=="" || $("#mi").val()=="" || $("#ln").val()=="") {
+    if($("#fn").val()=="" || $("#ln").val()=="") {
+      var paragraph = document.getElementById("name");
+      var text = document.createTextNode("****");
+      paragraph.appendChild(text);
       $("#name").addClass("error");
 
       return;
     } else {
       $("#name").removeClass("error");
+      var paragraph = document.getElementById("name");
+      var no_stars = document.createTextNode("Name: ")
+      paragraph.replaceWith(no_stars)
     }
 
     if($("#em").val()=="") {
       $("#email").addClass("error");
+      var paragraph = document.getElementById("email");
+      var text = document.createTextNode("****");
+      paragraph.appendChild(text);
 
       return;
     } else {
-      $("#name").removeClass("error");
+      $("#email").removeClass("error");
+      var paragraph = document.getElementById("email");
+      var no_stars = document.createTextNode("Email: ")
+      paragraph.replaceWith(no_stars)
     }
 
     alert("Thank you!  Please watch your email for our exciting newsletter and offers!");
